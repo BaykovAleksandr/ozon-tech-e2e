6b21342b2420844a2feb3936b6dffa40af954ef3
Baykov Aleksandr <baykov-1988@mail.ru>
2026-02-23 12:57:19 +0300

    fixed gitlab-ci.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
         viewport: { width: 1920, height: 1080 },
         userAgent:
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        trace: 'on-first-retry',
       },
     },
   ],
